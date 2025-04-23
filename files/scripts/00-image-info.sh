d860b0f433730548304c904582d672fad6cb3e95
Adam Fidel <adam@fidel.id>
2025-04-23 12:08:47 -0500

    chore: rename image to blue7

---
+++ files/scripts/00-image-info.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+set -ouex pipefail
+
+IMAGE_VENDOR="winblues"
+IMAGE_NAME="blue7"
+IMAGE_PRETTY_NAME="Blue7"
+IMAGE_LIKE="fedora"
+HOME_URL="https://blues.win/7"
+DOCUMENTATION_URL="https://blues.win/7/docs"
+SUPPORT_URL="https://github.com/winblues/blue7/issues"
+BUG_SUPPORT_URL="https://github.com/winblues/blue7/issues"
+
+IMAGE_INFO="/usr/share/ublue-os/image-info.json"
+IMAGE_REF="ostree-image-signed:docker://ghcr.io/$IMAGE_VENDOR/$IMAGE_NAME"
+
+FEDORA_MAJOR_VERSION=$(awk -F= '/VERSION_ID/ {print $2}' /etc/os-release)
+BASE_IMAGE_NAME="Aurora $FEDORA_MAJOR_VERSION"
+BASE_IMAGE="ghcr.io/ublue-os/aurora-dx"
+
+cat >$IMAGE_INFO <<EOF
+{
+  "image-name": "$IMAGE_NAME",
+  "image-vendor": "$IMAGE_VENDOR",
+  "image-ref": "$IMAGE_REF",
+  "image-tag":"latest",
+  "base-image-name": "$BASE_IMAGE",
+  "fedora-version": "$FEDORA_MAJOR_VERSION"
+}
+EOF
+
+# OS Release File
+sed -i "s/^VARIANT_ID=.*/VARIANT_ID=$IMAGE_NAME/" /usr/lib/os-release
+sed -i "s/^PRETTY_NAME=.*/PRETTY_NAME=\"Winblues 7 (FROM ${BASE_IMAGE_NAME^})\"/" /usr/lib/os-release
+sed -i "s/^NAME=.*/NAME=\"$IMAGE_PRETTY_NAME\"/" /usr/lib/os-release
+sed -i "s/^ID=.*/ID=\"$IMAGE_NAME\"/" /usr/lib/os-release
+sed -i "s|^HOME_URL=.*|HOME_URL=\"$HOME_URL\"|" /usr/lib/os-release
+sed -i "s|^DOCUMENTATION_URL=.*|DOCUMENTATION_URL=\"$DOCUMENTATION_URL\"|" /usr/lib/os-release
+sed -i "s|^SUPPORT_URL=.*|SUPPORT_URL=\"$SUPPORT_URL\"|" /usr/lib/os-release
+sed -i "s|^BUG_REPORT_URL=.*|BUG_REPORT_URL=\"$BUG_SUPPORT_URL\"|" /usr/lib/os-release
+sed -i "s|^CPE_NAME=\"cpe:/o:fedoraproject:fedora|CPE_NAME=\"cpe:/o:universal-blue:${IMAGE_PRETTY_NAME,}|" /usr/lib/os-release
+sed -i "s/^DEFAULT_HOSTNAME=.*/DEFAULT_HOSTNAME=\"${IMAGE_PRETTY_NAME,}\"/" /usr/lib/os-release
+sed -i "s/^ID=fedora/ID=${IMAGE_PRETTY_NAME,}\nID_LIKE=\"${IMAGE_LIKE}\"/" /usr/lib/os-release
