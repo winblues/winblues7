35e3b50e29ec32560b8f6369818d2dc0acd92ee1
Adam Fidel <adam@fidel.id>
2025-05-11 17:24:19 -0500

    remove file for default wallpaper

--- files/scripts/13-branding.sh
+++ files/scripts/13-branding.sh
@@ -1,2 +1,7 @@
+#!/bin/bash
+
+cat >> /usr/share/plasma/look-and-feel/authui7/contents/defaults << 'EOF'
 [Containments][1][Wallpaper][org.kde.image][General]
 Image=file:///usr/share/wallpapers/winblues7/contents/images/wallpaper.jxl
+FillMode=2
+EOF
