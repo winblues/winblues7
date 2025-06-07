0da25f4dfbea7a684a05f09107fb67710edce5dc
Adam Fidel <adam@fidel.id>
2025-06-07 09:30:05 -0500

    chore: update RPM version

--- files/scripts/10-aero-theme.sh
+++ files/scripts/10-aero-theme.sh
@@ -2,18 +2,19 @@
 
 set -exuo pipefail
 
-VERSION=0-0.1.20250511git47f9312.fc42
+VERSION=0-0.1.20250607git3f17650.fc42
 URL=https://github.com/winblues/blue7/releases/download/aerothemeplasma-${VERSION}/aerothemeplasma-${VERSION}.x86_64.rpm
 
 curl -L -o /tmp/aerothemeplasma.rpm "$URL"
 
 mkdir -p /tmp/winblues7-overlay
 
-cd /tmp/winblues7-overlay && \
-    rpm2cpio /tmp/aerothemeplasma.rpm | cpio -idmv
+cd /tmp/winblues7-overlay &&
+  rpm2cpio /tmp/aerothemeplasma.rpm | cpio -idmv
 
-cp -rf /tmp/winblues7-overlay/usr/* /usr/ && \
-    cp -rf /tmp/winblues7-overlay/etc/* /etc/ && \
-    rm -rf /tmp/winblues7-overlay /tmp/*.rpm
+cp -rf /tmp/winblues7-overlay/usr/* /usr/ &&
+  cp -rf /tmp/winblues7-overlay/etc/* /etc/ &&
+  rm -rf /tmp/winblues7-overlay /tmp/*.rpm
 
 kbuildsycoca6
+
